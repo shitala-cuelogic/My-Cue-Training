ffabe333f9f0ab7609f939b8a110c3a1f854de9b
shitala-cuelogic <shital.agarwal@cuelogic.co.in>
2016-01-21 20:01:08 +0530

    Changed assignment name.

--- Assignment-2/blocking-example.js
+++
@@ -1,8 +0,0 @@
-//Synchronous
-
-var fs = require("fs");
-
-var data = fs.readFileSync('input.txt');
-
-console.log(data.toString());
-console.log("Program Ended after completion for file operation."); //This is block until file operation is not completely done.
